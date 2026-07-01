31482fc70074022f5624a96801e56ad19c6ef55a
Novoh .K. Sada <kelvinsada23@gmail.com>
2026-07-01 22:55:45 +0100

    tidy up the code

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,17 +1,7 @@
-import { useEffect, useState, type ChangeEvent } from "react"
-import { useParams } from "react-router-dom"
-import React from 'react'
-import {AdvancedImage} from '@cloudinary/react';
-import {Cloudinary} from "@cloudinary/url-gen";
+import { useState, type ChangeEvent } from "react"
 
 const User = () => {
   const [displayImage,setDisplayImage] = useState<string|undefined>(undefined)
-  const [editImage,setEditImage ] = useState({
-    url:displayImage,
-    removeBackground:false,
-    changeBackgroundImage:false,
-    backgroundcolor:""
-  })
 
   const alterBackground = () =>{
     console.log("clicked")
@@ -23,21 +13,8 @@ const User = () => {
     })
   }
   const cloudinaryName = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
-  const cloudinaryApiKey = import.meta.env.VITE_MY_CLOUDINARY_API_KEY
-  const cloudinaryApiSecret = import.meta.env.VITE_MY_CLOUDINARY_API_SECRET
 
 
-  // Create a Cloudinary instance and set your cloud name.
-  const cld = new Cloudinary({
-    cloud: {
-      cloudName: 'demo'
-    }
-  });
-
-  // cld.image returns a CloudinaryImage with the configuration set.
-  const myImage = cld.image('sample');
-
-  // The URL of the image is: https://res.cloudinary.com/demo/image/upload/sample
 
   const handlePictureUpload= async (event:ChangeEvent<HTMLInputElement>)=>{
     const imageDetail = event.target.files
@@ -66,17 +43,15 @@ const User = () => {
   }
   return (
     <div>
-      {/* <p>User Page</p> */}
-      {/* <AdvancedImage cldImg={myImage} /> */}
-
       <div>
-        <p>cloud name: {import.meta.env.VITE_CLOUDINARY_CLOUD_NAME}</p>
+        {/* Upload the Image */}
         <label htmlFor="myImage">
           {/* <img src={cloudImage} alt=""/> */}
           <p>Drag and Drop or click to upload</p>
           <input id="myImage" type="file" onChange={(e:ChangeEvent<HTMLInputElement>)=>handlePictureUpload(e)}/>
         </label>
 
+        {/* Display the uploaded Item */}
         <div className="m-4 p-4 border border-gray-200 rounded-lg shadow-md bg-white max-w-xs">
           <div className="flex flex-col items-center gap-3">
             <img 
@@ -92,7 +67,7 @@ const User = () => {
           </div>
         </div>
 
-        {/* Adjust video settings */}
+        {/* Remove background settings */}
         <button onClick={alterBackground} className="bg-amber-600 m-4"> Take off background</button>
       </div>
     </div>
